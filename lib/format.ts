d52f068a3cbf385a158baa76631bbdfe9e440da6
Enrique Benitez <enriquebenitezjr@gmail.com>
2019-06-15 17:36:37 -0500

    refactor: run regex once

--- lib/format.ts
+++ lib/format.ts
@@ -5,16 +5,18 @@ import { Format } from './types' // eslint-disable-line no-unused-vars
 const capitalize = (string: string): string => string.charAt(0).toUpperCase() + string.slice(1)
 
 export default ({ type, subject, body }: Format) => {
-  if (subject.replace(/\.+$/, '').length === 0) {
+  const formattedSubject = subject.replace(/\.+$/, '')
+
+  if (formattedSubject.length === 0) {
     throw new Error('Commit subject must be at least 1 character long.\n')
   }
 
-  if (`${type}: ${subject.replace(/\.+$/, '')}`.length > 50) {
+  if (`${type}: ${formattedSubject}`.length > 50) {
     throw new Error('Commit type + subject must be 50 or less characters long.\n')
   }
 
   const commit = {
-    short: `${type}: ${subject.replace(/\.+$/, '')}`.toLowerCase(),
+    short: `${type}: ${formattedSubject}`.toLowerCase(),
     long: body ? wrap(capitalize(body), 100) + '.' : null
   }
 
