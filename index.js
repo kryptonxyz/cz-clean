e641b8f2f60323ccecfde0c84efc5cc96f63d5ef
Enrique Benitez <enriquebenitezjr@gmail.com>
2019-06-15 00:38:43 -0500

    feat: add prettier, xo and husky

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-const filter = array => array.filter(x => x);
+const filter = array => array.filter(x => x)
 
 module.exports = {
   prompter: (cz, commit) => {
@@ -8,24 +8,26 @@ module.exports = {
       {
         type: 'list',
         name: 'type',
-        message: `Select the type of change that you're committing:`,
-        choices: ['feat', 'fix', 'perf', 'chore'],
+        message: "Select the type of change that you're committing:",
+        choices: ['feat', 'fix', 'perf', 'chore']
       },
       {
         type: 'input',
         name: 'subject',
-        message: 'Write a short, imperative tense description of the change:',
+        message: 'Write a short, imperative tense description of the change:'
       },
       {
         type: 'input',
         name: 'body',
-        message: 'Provide a longer description of the change: (press enter to skip)\n',
-      },
-    ]).then(({ type, subject, body }) => {
-      const short = `${type}: ${subject}`
-      const long = body ? body : null
+        message: 'Provide a longer description of the change: (press enter to skip)\n'
+      }
+    ])
+      .then(({ type, subject, body }) => {
+        const short = `${type}: ${subject}`
+        const long = body ? body : null
 
-      commit(filter([short, long ? long : null]).join('\n\n'));
-    }).catch(e => console.log('\n\n💥 ', e.message));
+        commit(filter([short, long ? long : null]).join('\n\n'))
+      })
+      .catch(error => console.log('\n\n💥', error.message))
   }
-};
+}
